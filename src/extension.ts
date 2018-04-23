933fdb4333f85b6f748b14c0964198ade39e6dab
Nick DeMayo <sunmorgus@gmail.com>
2018-04-23 13:21:29 -0400

    - First working version;

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,17 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { SymbolInformation, Range } from 'vscode';
+import { ExtensionContext, commands, window } from 'vscode';
+import { SymbolsTreeDataProvider } from './symbolsTreeDataProvider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(
-        'Congratulations, your extension "vscode-codespade" is now active!'
-    );
+export function activate(context: ExtensionContext) {
+    console.log('Extension activated!!');
+    const symbolsTreeDataProvider = new SymbolsTreeDataProvider(context);
+    context.subscriptions.push(window.registerTreeDataProvider('symbolExplorer', symbolsTreeDataProvider));
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand(
-        'extension.sayHello',
-        async () => {
-            // The code you place here will be executed every time your command is executed
+    const navigateSymbolCommand = commands.registerCommand('symbolExplorer.navigateSymbol', range => { symbolsTreeDataProvider.select(range); });
+    context.subscriptions.push(navigateSymbolCommand);
 
-            const { activeTextEditor } = vscode.window;
-
-            if (activeTextEditor && activeTextEditor.document.uri) {
-                let symbols: Array<SymbolInformation> | undefined = await vscode.commands.executeCommand<Array<SymbolInformation>>(
-                    'vscode.executeDocumentSymbolProvider',
-                    activeTextEditor.document.uri
-                );
-
-                if (symbols && symbols.length > 0) {
-
-                }
-
-                // Display a message box to the user
-                vscode.window.showInformationMessage('Hello World!');
-            }
-        }
-    );
-
-    context.subscriptions.push(disposable);
+    const refreshSymbolCommand = commands.registerCommand('symbolExplorer.refresh', () => symbolsTreeDataProvider.refresh());
+    context.subscriptions.push(refreshSymbolCommand);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/symbolsTreeDataProvider.ts
@@ -0,0 +1,174 @@
+'use strict';
+
+import * as vscode from "vscode";
+import * as path from 'path';
+
+export enum SymbolKind {
+    File = 0,
+    Module = 1,
+    Namespace = 2,
+    Package = 3,
+    Class = 4,
+    Method = 5,
+    Property = 6,
+    Field = 7,
+    Constructor = 8,
+    Enum = 9,
+    Interface = 10,
+    Function = 11,
+    Variable = 12,
+    Constant = 13,
+    String = 14,
+    Number = 15,
+    Boolean = 16,
+    Array = 17,
+    Object = 18,
+    Key = 19,
+    Null = 20,
+    EnumMember = 21,
+    Struct = 22,
+    Event = 23,
+    Operator = 24,
+    TypeParameter = 25
+}
+
+export class SymbolsTreeDataProvider implements vscode.TreeDataProvider<SymbolTreeViewItem> {
+    private _onDidChangeTreeData: vscode.EventEmitter<SymbolTreeViewItem | undefined> = new vscode.EventEmitter<SymbolTreeViewItem | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<SymbolTreeViewItem | undefined> = this._onDidChangeTreeData.event;
+
+    private autoRefresh: boolean = true;
+    private editor: vscode.TextEditor;
+
+    constructor(private context: vscode.ExtensionContext) {
+        vscode.window.onDidChangeActiveTextEditor(() => this.onActiveEditorChanged());
+        vscode.workspace.onDidSaveTextDocument(() => this.onDocumentChanged());
+        this.onActiveEditorChanged();
+    }
+
+    refresh(): void {
+        this.editor = vscode.window.activeTextEditor;
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: SymbolTreeViewItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(): Thenable<SymbolTreeViewItem[]> {
+        return new Promise(resolve => {
+            resolve(this.getSymbolsForActiveEditor());
+        });
+    }
+
+    select(range: vscode.Range) {
+        this.editor.selection = new vscode.Selection(range.start, range.end);
+        vscode.commands.executeCommand<Array<vscode.SymbolInformation>>(
+            'revealLine',
+            {
+                lineNumber: range.start.line,
+                at: 'center'
+            }
+        );
+    }
+
+    private onActiveEditorChanged(): void {
+        if (vscode.window.activeTextEditor) {
+            if (vscode.window.activeTextEditor.document.uri.scheme === 'file') {
+                this.refresh();
+            }
+        }
+    }
+
+    private onDocumentChanged(): void {
+        if (this.autoRefresh)
+            this._onDidChangeTreeData.fire();
+    }
+
+    private async getSymbolsForActiveEditor(): Promise<SymbolTreeViewItem[]> {
+        let symbolsTreeViewItems: SymbolTreeViewItem[];
+
+        if (this.editor && this.editor.document.uri) {
+            let symbols: Array<vscode.SymbolInformation> | undefined = await vscode.commands.executeCommand<Array<vscode.SymbolInformation>>(
+                'vscode.executeDocumentSymbolProvider',
+                this.editor.document.uri
+            );
+
+            const toSymbol = (symbol: vscode.SymbolInformation): SymbolTreeViewItem => {
+                return new SymbolTreeViewItem(symbol.name, symbol.kind, symbol.location, vscode.TreeItemCollapsibleState.None, this.context, {
+                    command: 'symbolExplorer.navigateSymbol',
+                    title: '',
+                    arguments: [symbol.location.range]
+                });
+            }
+
+            symbolsTreeViewItems = symbols.map(symbol => toSymbol(symbol));
+        }
+        else {
+            const noSymbols = new SymbolTreeViewItem("No symbols found in file", 0, undefined, vscode.TreeItemCollapsibleState.None, this.context);
+            symbolsTreeViewItems.push(noSymbols);
+        }
+
+        return symbolsTreeViewItems;
+    }
+}
+
+class SymbolTreeViewItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        private kind: number,
+        public readonly location: vscode.Location | undefined,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        private context: vscode.ExtensionContext,
+        public readonly command?: vscode.Command
+    ) {
+        super(label, collapsibleState);
+    }
+
+    get tooltip(): string {
+        return `${this.label}-${this.kind}`
+    }
+
+    private getIconPath(): { light: string, dark: string } {
+        const name = this.toCssClassName(this.kind);
+        const iconPaths = {
+            light: this.context.asAbsolutePath(path.join('resources', 'light', `${name}.svg`)),
+            dark: this.context.asAbsolutePath(path.join('resources', 'dark', `${name}.svg`))
+        };
+        return iconPaths;
+    }
+
+    iconPath = this.getIconPath();
+
+    contextValue = 'symbolTreeViewItem'
+
+    private toCssClassName(kind: SymbolKind): string {
+        const _fromMapping: { [n: number]: string } = Object.create(null);
+        _fromMapping[SymbolKind.File] = 'file';
+        _fromMapping[SymbolKind.Module] = 'object';
+        _fromMapping[SymbolKind.Namespace] = 'object';
+        _fromMapping[SymbolKind.Package] = 'object';
+        _fromMapping[SymbolKind.Class] = 'class';
+        _fromMapping[SymbolKind.Method] = 'method';
+        _fromMapping[SymbolKind.Property] = 'property';
+        _fromMapping[SymbolKind.Field] = 'field';
+        _fromMapping[SymbolKind.Constructor] = 'method';
+        _fromMapping[SymbolKind.Enum] = 'enum';
+        _fromMapping[SymbolKind.Interface] = 'interface';
+        _fromMapping[SymbolKind.Function] = 'method';
+        _fromMapping[SymbolKind.Variable] = 'field';
+        _fromMapping[SymbolKind.Constant] = 'constant';
+        _fromMapping[SymbolKind.String] = 'key';
+        _fromMapping[SymbolKind.Number] = 'number';
+        _fromMapping[SymbolKind.Boolean] = 'null';
+        _fromMapping[SymbolKind.Array] = 'array';
+        _fromMapping[SymbolKind.Object] = 'object';
+        _fromMapping[SymbolKind.Key] = 'key';
+        _fromMapping[SymbolKind.Null] = 'null';
+        _fromMapping[SymbolKind.EnumMember] = 'enum-member';
+        _fromMapping[SymbolKind.Struct] = 'struct';
+        _fromMapping[SymbolKind.Event] = 'event';
+        _fromMapping[SymbolKind.Operator] = 'operator';
+        _fromMapping[SymbolKind.TypeParameter] = 'type-parameter';
+        return _fromMapping[kind] || 'property';
+    };
+}
